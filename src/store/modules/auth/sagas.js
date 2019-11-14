c45b3914dec4e681c9edb49fbfbaef4cc9fcdfd1
kleyson castro <kleysoncastro99@gmail.com>
2019-11-13 21:03:57 -0300

    chore: massage using react-toastity

--- src/store/modules/auth/sagas.js
+++ src/store/modules/auth/sagas.js
@@ -1,27 +1,33 @@
 import { all, takeLatest, call, put } from 'redux-saga/effects';
+import { toast } from 'react-toastify';
 import api from '~/services/api';
 import history from '~/services/history';
 
-import { sigInSuccess } from './actions';
+import { sigInSuccess, signFailure } from './actions';
 
 export function* signIn({ payload }) {
-  const { email, password } = payload;
+  try {
+    const { email, password } = payload;
 
-  const response = yield call(api.post, '/sessions', {
-    email,
-    password,
-  });
+    const response = yield call(api.post, '/sessions', {
+      email,
+      password,
+    });
 
-  const { token, user } = response.data;
+    const { token, user } = response.data;
 
-  if (!user.provider) {
-    console.tron.error('usuariario nao é prestador');
-    return;
+    if (!user.provider) {
+      toast.error('usuariario nao é prestador');
+      return;
+    }
+
+    yield put(sigInSuccess(token, user));
+
+    history.push('/dashboard');
+  } catch (errr) {
+    toast.error('Usuaário ou senha inválidos');
+    yield put(signFailure());
   }
-
-  yield put(sigInSuccess(token, user));
-
-  history.push('/dashboard');
 }
 
 export default all([takeLatest('@auth/SIGN_IN_REQUEST', signIn)]);
