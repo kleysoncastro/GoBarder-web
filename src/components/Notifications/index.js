c10cc0f16b161b4fd54713da06fd345652d230bb
kleyson castro <kleysoncastro99@gmail.com>
2019-11-18 19:26:11 -0300

    chore: style nitification

--- src/components/Notifications/index.js
+++ src/components/Notifications/index.js
@@ -1,14 +1,64 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { MdNotifications } from 'react-icons/md';
 
-import { Container, Badge } from './styles';
+import {
+  Scroll,
+  Container,
+  Badge,
+  Notification,
+  NotificationList,
+} from './styles';
 
 export default function Notifications() {
+  const [visible, setVisible] = useState(false);
+
+  function handleToggleVisible() {
+    setVisible(!visible);
+  }
+
   return (
     <Container>
-      <Badge>
+      <Badge onClick={handleToggleVisible} herUnread>
         <MdNotifications color="#7159c1" size={20} />
       </Badge>
+
+      <NotificationList visible={visible}>
+        <Scroll>
+          <Notification unread>
+            <p>Voce possui um novo agendamento</p>
+            <time>Há 2 dias </time>
+
+            <button type="button">Marcar como lida</button>
+          </Notification>
+
+          <Notification>
+            <p>Voce possui um novo agendamento</p>
+            <time>Há 2 dias </time>
+
+            <button type="button">Marcar como lida</button>
+          </Notification>
+
+          <Notification>
+            <p>Voce possui um novo agendamento</p>
+            <time>Há 2 dias </time>
+
+            <button type="button">Marcar como lida</button>
+          </Notification>
+
+          <Notification>
+            <p>Voce possui um novo agendamento</p>
+            <time>Há 2 dias </time>
+
+            <button type="button">Marcar como lida</button>
+          </Notification>
+
+          <Notification>
+            <p>Voce possui um novo agendamento</p>
+            <time>Há 2 dias </time>
+            <button type="button">Marcar como lida</button>
+          </Notification>
+        </Scroll>
+      </NotificationList>
     </Container>
   );
 }
