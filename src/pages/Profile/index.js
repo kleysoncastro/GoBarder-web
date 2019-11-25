522f854265df4a8da4bd24fa1943c1e6ad4c6f5d
kleyson castro <kleysoncastro99@gmail.com>
2019-11-25 10:19:49 -0300

    chore: logout for application

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -3,6 +3,7 @@ import { useSelector, useDispatch } from 'react-redux';
 import { Form, Input } from '@rocketseat/unform';
 
 import { updateProfileRequest } from '~/store/modules/user/actions';
+import { signOut } from '~/store/modules/auth/actions';
 
 import AvatarInput from './AvatarInput';
 
@@ -14,6 +15,10 @@ export default function Profile() {
 
   function handleSubmit(data) {
     dispatch(updateProfileRequest(data));
+  }
+
+  function handleSignOut() {
+    dispatch(signOut());
   }
 
   return (
@@ -43,7 +48,9 @@ export default function Profile() {
         />
         <button type="submit">Atualizar Perfil</button>
       </Form>
-      <button type="button">Sair do Gobarber</button>
+      <button onClick={handleSignOut} type="button">
+        Sair do Gobarber
+      </button>
     </Container>
   );
 }
